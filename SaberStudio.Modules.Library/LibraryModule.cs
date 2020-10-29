8dc8d763f54c8b20a7ac38729d7c24a8fee68ff2
Baeumli <lbaumgartner@hotmail.de>
2020-10-29 21:13:43 +0100

    Create library module

---
+++ SaberStudio.Modules.Library/LibraryModule.cs
@@ -0,0 +1,20 @@
+﻿using SaberStudio.Modules.Library.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace SaberStudio.Modules.Library
+{
+    public class LibraryModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
